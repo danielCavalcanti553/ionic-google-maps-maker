34bc485d5aec06dae1cabeb958cb57f1f1cd83be
aula <aula@aula.com.br>
2019-02-20 19:36:23 -0300

    version 1

---
+++ src/pages/list/list.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, IonicPage } from 'ionic-angular';
+@IonicPage()
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+ 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  
+}
